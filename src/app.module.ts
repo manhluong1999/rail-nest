5ad36119a56e64b796a1100ec8d215079271d704
Daniel Olabemiwo <olabemiwodaniel@gmail.com>
2024-10-22 18:28:25 +0100

    chore: add in-memory cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+	  CacheModule.register({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
